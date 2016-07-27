611ac85681bb28e5702d4ebff4dff5713e117a96
Scott Alexander-Bown <dev@scottyab.com>
2016-07-27 13:45:27 +0100

    icon vector thumb to try, and code reformatting

--- SlideToUnlock/src/main/java/com/slidetounlock/MainActivity.java
+++ SlideToUnlock/src/main/java/com/slidetounlock/MainActivity.java
@@ -3,26 +3,30 @@ package com.slidetounlock;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
+
 import com.slidetounlock.widget.R;
 import com.slidetounlock.widget.SlideToUnlock;
 
 public class MainActivity extends Activity implements SlideToUnlock.OnUnlockListener {
 
-  private SlideToUnlock slideToUnlock;
+    private SlideToUnlock slideToUnlock;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    slideToUnlock = (SlideToUnlock) findViewById(R.id.slidetounlock);
-    slideToUnlock.setOnUnlockListener(this);
-  }
+        slideToUnlock = (SlideToUnlock) findViewById(R.id.slidetounlock);
+        slideToUnlock.setOnUnlockListener(this);
+        //slideToUnlock.setText("Slide to Pause");
+    }
 
 
-  @Override
-  public void onUnlock() {
-    Toast.makeText(this, "Unlocked", Toast.LENGTH_SHORT).show();
-  }
+    @Override
+    public void onUnlock() {
+        Toast.makeText(this, "Unlocked", Toast.LENGTH_SHORT).show();
+        slideToUnlock.reset();
+
+    }
 
 }
--- SlideToUnlock/src/main/java/com/slidetounlock/widget/SlideToUnlock.java
+++ SlideToUnlock/src/main/java/com/slidetounlock/widget/SlideToUnlock.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package com.slidetounlock.widget;
 
 import android.animation.ObjectAnimator;
@@ -39,146 +39,145 @@ import android.widget.TextView;
  */
 public class SlideToUnlock extends RelativeLayout {
 
-  private Drawable track;
-  private View     background;
+    private Drawable track;
+    private View background;
 
-  public interface OnUnlockListener {
+    public interface OnUnlockListener {
+
+        /**
+         * Called when unlock event occurred.
+         */
+        void onUnlock();
+    }
+
+    private OnUnlockListener listener;
+    private SeekBar seekbar;
+    private TextView label;
+    private int thumbWidth;
+
+    public SlideToUnlock(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public SlideToUnlock(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public SlideToUnlock(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs);
+    }
+
+    public void setOnUnlockListener(OnUnlockListener listener) {
+        this.listener = listener;
+    }
 
     /**
-     * Called when unlock event occurred.
+     * Resets slider to initial state.
      */
-    void onUnlock();
-  }
-
-  private OnUnlockListener listener;
-  private SeekBar          seekbar;
-  private TextView         label;
-  private int              thumbWidth;
-
-  public SlideToUnlock(Context context) {
-    super(context);
-    init(context, null);
-  }
-
-  public SlideToUnlock(Context context, AttributeSet attrs) {
-    super(context, attrs);
-    init(context, attrs);
-  }
-
-  public SlideToUnlock(Context context, AttributeSet attrs, int defStyle) {
-    super(context, attrs, defStyle);
-    init(context, attrs);
-  }
-
-  public void setOnUnlockListener(OnUnlockListener listener) {
-    this.listener = listener;
-  }
-
-  /**
-   * Resets slider to initial state.
-   */
-  public void reset() {
-    seekbar.setProgress(0);
-  }
-
-  private void init(Context context, AttributeSet attrs) {
-    if (isInEditMode()) {
-      return;
-    }
-    LayoutInflater inflater = (LayoutInflater) context
-        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    inflater.inflate(R.layout.slidetounlock_lt, this, true);
-
-    label = (TextView) findViewById(R.id.slider_label);
-    seekbar = (SeekBar) findViewById(R.id.slider_seekbar);
-    background = findViewById(R.id.slider_bg);
-
-    TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.SlideToUnlockView);
-    String text = attributes.getString(R.styleable.SlideToUnlockView_text);
-    Drawable thumb = attributes.getDrawable(R.styleable.SlideToUnlockView_thumb);
-    if (thumb == null) {
-      thumb = getResources().getDrawable(R.drawable.slidetounlock_thumb);
-    }
-    track = attributes.getDrawable(R.styleable.SlideToUnlockView_track);
-    attributes.recycle();
-
-    thumbWidth = thumb.getIntrinsicWidth();
-
-    if (track != null) {
-      background.setBackgroundDrawable(track);
+    public void reset() {
+        seekbar.setProgress(0);
     }
 
-    if (text != null) {
-      label.setText(text);
-    }
-    label.setPadding(thumbWidth, 0, 0, 0);
-
-    int defaultOffset = seekbar.getThumbOffset();
-    seekbar.setThumb(thumb);
-    seekbar.setThumbOffset(defaultOffset);
-
-
-    seekbar.setOnTouchListener(new OnTouchListener() {
-      private boolean isInvalidMove;
-
-      @Override
-      public boolean onTouch(View view, MotionEvent motionEvent) {
-        switch (motionEvent.getAction()) {
-          case MotionEvent.ACTION_DOWN:
-            return isInvalidMove = motionEvent.getX() > thumbWidth;
-          case MotionEvent.ACTION_MOVE:
-            return isInvalidMove;
-          case MotionEvent.ACTION_UP:
-            return isInvalidMove;
+    private void init(Context context, AttributeSet attrs) {
+        if (isInEditMode()) {
+            return;
         }
-        return false;
-      }
-    });
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.slidetounlock_lt, this, true);
 
-    seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+        label = (TextView) findViewById(R.id.slider_label);
+        seekbar = (SeekBar) findViewById(R.id.slider_seekbar);
+        background = findViewById(R.id.slider_bg);
 
-      @Override
-      public void onStartTrackingTouch(SeekBar seekBar) {
-      }
-
-      @Override
-      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
-        label.setAlpha(1f - progress * 0.02f);
-      }
-
-      @Override
-      public void onStopTrackingTouch(SeekBar seekBar) {
-        if (seekBar.getProgress() < 100) {
-          ObjectAnimator anim = ObjectAnimator.ofInt(seekBar, "progress", 0);
-          anim.setInterpolator(new AccelerateDecelerateInterpolator());
-          anim.setDuration(getResources().getInteger(android.R.integer.config_shortAnimTime));
-          anim.start();
+        TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.SlideToUnlockView);
+        String text = attributes.getString(R.styleable.SlideToUnlockView_text);
+        Drawable thumb = attributes.getDrawable(R.styleable.SlideToUnlockView_thumb);
+        if (thumb == null) {
+            thumb = getResources().getDrawable(R.drawable.slidetounlock_thumb);
         }
-        else {
-          if (listener != null) {
-            listener.onUnlock();
-          }
-        }
-      }
-    });
-  }
+        track = attributes.getDrawable(R.styleable.SlideToUnlockView_track);
+        attributes.recycle();
 
-  @Override
-  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    if (isInEditMode()) {
-      return;
+        thumbWidth = thumb.getIntrinsicWidth();
+
+        if (track != null) {
+            background.setBackgroundDrawable(track);
+        }
+
+        if (text != null) {
+            label.setText(text);
+        }
+        label.setPadding(thumbWidth, 0, 0, 0);
+
+        int defaultOffset = seekbar.getThumbOffset();
+        seekbar.setThumb(thumb);
+        seekbar.setThumbOffset(defaultOffset);
+
+
+        seekbar.setOnTouchListener(new OnTouchListener() {
+            private boolean isInvalidMove;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        return isInvalidMove = motionEvent.getX() > thumbWidth;
+                    case MotionEvent.ACTION_MOVE:
+                        return isInvalidMove;
+                    case MotionEvent.ACTION_UP:
+                        return isInvalidMove;
+                }
+                return false;
+            }
+        });
+
+        seekbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+                label.setAlpha(1f - progress * 0.02f);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (seekBar.getProgress() < 100) {
+                    ObjectAnimator anim = ObjectAnimator.ofInt(seekBar, "progress", 0);
+                    anim.setInterpolator(new AccelerateDecelerateInterpolator());
+                    anim.setDuration(getResources().getInteger(android.R.integer.config_shortAnimTime));
+                    anim.start();
+                } else {
+                    if (listener != null) {
+                        listener.onUnlock();
+                    }
+                }
+            }
+        });
     }
 
-    //prevents 9-patch background image from full size stretching
-    if (getLayoutParams().height == ViewGroup.LayoutParams.WRAP_CONTENT) {
-      background.getLayoutParams().height = seekbar.getHeight() + fromDpToPx(3);
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (isInEditMode()) {
+            return;
+        }
+
+        //prevents 9-patch background image from full size stretching
+        if (getLayoutParams().height == ViewGroup.LayoutParams.WRAP_CONTENT) {
+            background.getLayoutParams().height = seekbar.getHeight() + fromDpToPx(3);
+        }
+
     }
 
-  }
-
-  public int fromDpToPx(int dp) {
-    return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
-        getResources().getDisplayMetrics());
-  }
+    public int fromDpToPx(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                getResources().getDisplayMetrics());
+    }
 }
